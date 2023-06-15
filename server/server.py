06953e3dfba0dda7d1533750dcd84dd2f1dd799a
Mario Aguilar Real <94491813+MarioAguilarReal@users.noreply.github.com>
2023-06-14 17:04:13 -0700

    Updates
    10/06/2023

--- server/server.py
+++ server/server.py
@@ -1,6 +1,8 @@
 import json
 from flask import Flask, request
 from config import db
+from bson import ObjectId
+from flask import abort
 
 app = Flask(__name__)
 
@@ -51,9 +53,28 @@ def get_products():
 def save_product():
     #Should save a new product
     product = request.get_json()
-    db.products.insert_one(product)
 
-    print(product)
+    #there should a title and it should at 3 chars
+    if "title" not in product or len(product['title']) < 3:
+        return abort(400, 'invalid title')
+
+    if "category" not in product or len(product['category']) < 1:
+        return abort(400, 'invalid category')
+
+    if "image" not in product or len(product['image']) < 1:
+        return abort(400, 'invalid image')
+
+
+    if 'price' not in product:
+        return abort(400, 'Price is required')
+
+    if not isinstance(product['price'], (int, float)):
+        return abort(400, 'Price should be a number')
+
+    if product['price'] > 0:
+        return abort(400, 'Price should be a greater than zero')
+
+    db.products.insert_one(product)
     return json.dumps(fix_id(product))
 
 
@@ -84,5 +105,94 @@ def get_categories():
             categories.append(prod["category"])
     categories.sort()
     return json.dumps(categories)
+
+
+@app.get("/api/products/category/<cat>")
+def get_prods_by_category(cat = None):
+
+    cursor = db.products.find({"category": cat})
+    products = []
+
+    for prod in cursor:
+        products.append(fix_id(prod))
+    return json.dumps(products)
+
+
+@app.get("/api/products/search/<title>")
+def get_products_by_title(title = None):
+
+    cursor = db.products.find({"title": {"$regex": title, "$options": "i"}})
+    products = []
+
+    for prod in cursor:
+        products.append(fix_id(prod))
+
+    return json.dumps(products)
+
+
+@app.get("/api/product/<id>")
+def get_product_by_id(id):
+    if not ObjectId.is_valid(id):
+        return abort(400, 'Invalid id')
+    db_id = ObjectId(id)
+    product = db.products.find_one({"_id":db_id})
+
+    if product is None:
+        return abort(404, "invalid id")
+
+    return json.dumps(fix_id(product))
+
+
+"""
+read all
+read by code
+save
+"""
+@app.get("/api/coupons")
+def get_coupons():
+    coupons = []
+
+    cursor = db.coupon.find({})
+    for coup in cursor:
+        coupons.append(fix_id(coup))
+    return json.dumps(f'{coupons}' )
+
+@app.post("/api/coupons")
+def save_coupon():
+    coupon = request.get_json()
+
+    if "code" not in coupon or len(coupon['code']) < 3:
+        return abort(400, 'Invalid code')
+
+    if "discount" not in coupon:
+        return abort(400, 'discount is required')
+
+    if not isinstance(coupon['discount'], (int)):
+        return abort(400, 'Discount should be a number')
+
+    if coupon['discount']>40 or coupon['discount']<0:
+        return abort(400, 'Discount invalid')
+
+    db.coupon.insert_one(coupon)
+
+    return json.dumps(fix_id(coupon))
+
+@app.get("/api/coupons/search/<code>")
+def get_coupons_by_code(code = None):
+
+    cursor = db.coupon.find({"code": code})
+    coupons = []
+
+    for coup in cursor:
+        coupons.append(fix_id(coup))
+
+    return json.dumps(coupons)
+
+
+"""
+code (required, length > 3)
+discount (required, numbers, greater than zero, lower than 40)
+"""
+
 
 app.run(debug = True)
